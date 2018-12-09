29ad2640046c6a1fb96e598a3124f734ca3fb570
Joao Wick <joaotorrez14@gmail.com>
2018-12-09 00:17:00 -0200

    Commit

---
+++ CapturaImagemWebCam/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picWebCam = new System.Windows.Forms.PictureBox();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.btnCaptura = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picWebCam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picWebCam
+            // 
+            this.picWebCam.BackColor = System.Drawing.Color.White;
+            this.picWebCam.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picWebCam.Location = new System.Drawing.Point(12, 22);
+            this.picWebCam.Margin = new System.Windows.Forms.Padding(4);
+            this.picWebCam.Name = "picWebCam";
+            this.picWebCam.Size = new System.Drawing.Size(581, 400);
+            this.picWebCam.TabIndex = 0;
+            this.picWebCam.TabStop = false;
+            // 
+            // picImagem
+            // 
+            this.picImagem.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picImagem.Location = new System.Drawing.Point(608, 22);
+            this.picImagem.Margin = new System.Windows.Forms.Padding(4);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(581, 400);
+            this.picImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picImagem.TabIndex = 1;
+            this.picImagem.TabStop = false;
+            // 
+            // btnCaptura
+            // 
+            this.btnCaptura.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCaptura.Location = new System.Drawing.Point(12, 436);
+            this.btnCaptura.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCaptura.Name = "btnCaptura";
+            this.btnCaptura.Size = new System.Drawing.Size(581, 55);
+            this.btnCaptura.TabIndex = 2;
+            this.btnCaptura.Text = "Capturar Imagem";
+            this.btnCaptura.UseVisualStyleBackColor = true;
+            this.btnCaptura.Click += new System.EventHandler(this.btnCaptura_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(608, 436);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(581, 55);
+            this.btnSalvar.TabIndex = 3;
+            this.btnSalvar.Text = "Salvar Imagem";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1206, 511);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnCaptura);
+            this.Controls.Add(this.picImagem);
+            this.Controls.Add(this.picWebCam);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Capturando Imagem da WebCam";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picWebCam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picWebCam;
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.Button btnCaptura;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
+
---
+++ CapturaImagemWebCam/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public DirectX.Capture.Filter Camera;
+        public DirectX.Capture.Capture CaptureInfo;
+        public DirectX.Capture.Filters CamContainer;
+        Image capturaImagem;
+        public string caminhoImagemSalva = null;
+  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           CamContainer = new DirectX.Capture.Filters();  
+           try
+           {
+             int no_of_cam = CamContainer.VideoInputDevices.Count;
+
+             for (int i = 0; i < no_of_cam; i++ )
+             {
+                try
+                {
+                        // obtém o dispositivo de entrada do vídeo
+                        Camera = CamContainer.VideoInputDevices[i];
+ 
+                        // inicializa a Captura usando o dispositivo
+                        CaptureInfo = new DirectX.Capture.Capture(Camera, null);
+                                                                        
+                        // Define a janela de visualização do vídeo
+                        CaptureInfo.PreviewWindow = this.picWebCam;
+ 
+                        // Capturando o tratamento de evento
+                        CaptureInfo.FrameCaptureComplete += AtualizaImagem;
+                                                 
+                        // Captura o frame do dispositivo
+                        CaptureInfo.CaptureFrame();
+                         
+                        // Se o dispositivo foi encontrado e inicializado então sai sem checar o resto
+                        break;
+                }
+                catch (Exception ex) 
+                {
+                    throw ex;    
+                }
+            }
+        }
+        catch (Exception ex)
+        {                
+            MessageBox.Show(this, ex.Message);
+        }
+     }
+
+        public void AtualizaImagem(PictureBox frame)
+        {
+            try
+            {
+                capturaImagem = frame.Image;
+                this.picImagem.Image = capturaImagem;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro " + ex.Message);
+            }
+        }
+       
+        private void btnCaptura_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CaptureInfo.CaptureFrame();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro " + ex.Message);
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                caminhoImagemSalva = @"c:\dados\" + "ImagemWebCam" + DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + DateTime.Now.Millisecond.ToString() + ".jpg";
+                picImagem.Image.Save(caminhoImagemSalva, ImageFormat.Jpeg);
+                MessageBox.Show("Imagem salva com sucesso");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Erro " + ex.Message);
+            }
+        }
+    }
+}
